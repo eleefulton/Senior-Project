275e117adb5a8ed3e3349673cdd29ddfdcc1f269
Ethan Fulton <eleefulton@gmail.com>
2018-05-07 16:01:58 -0400

    compute term frequency using categories

--- tfidf.c
+++ tfidf.c
@@ -1,17 +1,23 @@
 /*
-  This program computes an array for each word found
-  in a series of documents. It also computes an array
-  that shows how many words there are total in each
-  document.
+  This program computes an array of each unique 
+  word found in a series of documents in a categoyr 
+  and the number of times that word appears in each category. 
+  It also computes an array for the lengths of each category
+  measured by total number of words in all documents in
+  that category.
 
-  If a word does not appear in a document, it's value
-  for the corresponding documents position in the array
-  will be 0.
+  The number of categories will be input as the first
+  command line argument, and then each category prefix
+  and the number of documents in that category will
+  follow, seperated by spaces.
 
-  The number of files in the series (all files must be
-  in the same directory and named using the scheme 
-  XYZ.txt) will be input as the first command line
-  argument.
+  Example Input: 2 b 5 e 7
+  means there are two categories, one with the prefix
+  'b' which has 5 documents in it and one with the prefix
+  'e' which has 7 documents in int
+
+  Max number of categories that can be input is 5
+
 */ 
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,10 +29,11 @@
 #define NUM_FILES 1
 #define MAX_FILES 999
 #define MAX_WORDS 100000
+#define MAX_CATEGORIES 5
 
 typedef struct{
   char string[50];
-  int count[MAX_FILES];
+  float term_frequency[MAX_CATEGORIES];
 }Word;
 
 // search array of found words for given string, if it is found
@@ -48,59 +55,100 @@ int in_list(Word found_words[], char *string, int num_words)
 
 int main(int argc, char *argv[])
 {
-  int num_files = atoi(argv[1]);
-  FILE *fp;
-  char *file_name, *string;
-  Word *found_words = malloc(MAX_WORDS * sizeof(Word));
-  int current_file_num = 1, total_words = 0, index, words_in_file = 0;
-  int words_in_files[num_files];
-
-  for(current_file_num; current_file_num <= num_files; current_file_num++)
+  int num_categories = atoi(argv[1]);
+  if(num_categories > MAX_CATEGORIES)
   {
-    file_name = build_name(current_file_num);                                   // build file name
-    fp = fopen(file_name, "r");                                                 // load file
-    if(fp == NULL)                                                              // check file opend properly
+    printf("Error: too many categories\n");
+    return -1;
+  }
+  printf("num categories %d\n", num_categories);
+  char *categories[num_categories];
+  int category_docs[num_categories];                                           // number of documents in each category
+  int category_lengths[num_categories];                                        // total number of words in all documents in category
+  int unique_words = 0;                                                        // number of unique words found
+  Word *found_words = malloc(MAX_WORDS * sizeof(Word));
+
+  for(int i = 0; i < num_categories; i++)
+  {
+    category_lengths[i] = 0;
+  }
+
+  for(int i = 0, c = 2; i < num_categories; i++, c+=2)
+  {
+    categories[i] = argv[c];
+    category_docs[i] = atoi(argv[c+1]);
+  }
+
+  for(int i = 0; i < num_categories; i++)
+  {
+    printf("%s: %d\n", categories[i], category_docs[i]);
+  }
+
+  for(int i = 0; i < num_categories; i++)
+  {
+    for(int j = 0; j < category_docs[i]; j++)
     {
-      printf("FILE NOT LOADED\n");
-      return -1;
-    }
-    while(!feof(fp))                                                            // loop through until end of file
-    {
-      string = parse_next_word(fp);                                             // parse next word
-      if(string[0] != '\0')                                                     // check word is not empty
+      int num_files = category_docs[i];
+      int current_file_num = 1;                                                // current file in set
+      int index;
+      int current_file_length = 0;                                             // length of current document (words)
+      char *file_name = malloc(sizeof(char)*9);
+      char *string;
+      FILE *fp;
+      strncat(file_name, categories[i], 1);
+      strncat(file_name, build_name(j+1), 7);
+      printf("%s\n", file_name);
+      fp = fopen(file_name, "r");                                              // load file
+
+      if(fp == NULL)                                                           // check file opened properly
       {
-        words_in_file++;                                                        // increment count of words in file
-        if((index = in_list(found_words, string, total_words)) >= 0)            // otherwise, check found_words[] for string
+        printf("Error: FILE NOT LOADED\n");
+        return -1;
+      }
+      while(!feof(fp))
+      {
+        string = parse_next_word(fp);                                          // parse next word
+        if(string[0] != '\0')
         {
-          found_words[index].count[current_file_num-1]++;                       // if in found_words[], increment count for this file
-        }
-        else                                                                    // otherwise, add string to found_words[]
-        {
-          total_words++;                                                        // increment count of total words
-          strncpy(found_words[total_words-1].string, string, 50);               // copy string to found_words[]
-          found_words[total_words-1].count[current_file_num-1]++;               // increment count for current file
+          current_file_length++;
+          if((index = in_list(found_words, string, unique_words)) >= 0)        // check found_words[] for string
+          {
+            found_words[index].term_frequency[i]++;                            // if in found_words[], increment count for this category
+          }
+          else                                                                 // otherwise, add string to found_words[]
+          {
+            unique_words++;                                                    // increment count of total unique words
+            strncpy(found_words[unique_words-1].string, string, 50);           // copy string to found_words[]
+            found_words[unique_words-1].term_frequency[i]++;                    // increment count for current category
+          }
         }
       }
+      fclose(fp);                                                              // close the file
+      free(file_name);
+      category_lengths[i] = category_lengths[i] + current_file_length;         // record length of this file (words)
+      current_file_length = 0;                                                 // reset current_file_length
     }
-    fclose(fp);                                                                 // close the file
-    words_in_files[current_file_num-1] = words_in_file;                         // record total number of words in this file
-    words_in_file = 0;
   }
 
-  printf("WORDS IN FILES: ");                                                   // print results
-  for(int k = 0; k < num_files; k++)
-  {
-    printf("[%d]", words_in_files[k]);
-  }
-  printf("\n");
-  for(int i = 0; i < total_words; i++)
-  {
-    printf("%s : ", found_words[i].string);
-    for(int j = 0; j < num_files; j++)
+  for(int i = 0; i < num_categories; i++)                                      // compute term frequency per category
+  {                                                                            // (term count across all docs / total words in category)
+    for(int j = 0; j < unique_words; j++)
     {
-      printf("[%d]", found_words[i].count[j]);
+      found_words[j].term_frequency[i] = 
+        (float)found_words[j].term_frequency[i] / category_lengths[i];
+    }
+  }
+
+  for(int i = 0; i < unique_words; i++)                                        // print term frequencies
+  {
+    printf("%s: ", found_words[i].string);
+    for(int j = 0;  j < num_categories; j++)
+    {
+      printf("[%f]", found_words[i].term_frequency[j]);
     }
     printf("\n");
   }
+
+
   return 0;
 }
